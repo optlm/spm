9f8109b9a9cff813881301b4f0de97901ceb687a
optlm <limyin2010@163.com>
2024-09-29 16:45:00 +0800

    1.3.11

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "EngageMinds",
-            url: "https://cdn.engageminds.ai/sdk/ios/PublisherSDK_EngageMinds_iOS_V1.3.10.zip",
-            checksum: "2f241cdf6b717928f504f991465809bf32de2fb60ba2dd2f5bbe00eee79fa003"
+            url: "https://cdn.engageminds.ai/sdk/ios/PublisherSDK_EngageMinds_iOS_V1.3.11.zip",
+            checksum: "7785a5b8d02c5ba62c849e832571a072fd5fedaa437c5b8f6e2a27a3604cb7c2"
         )
     ]
 )
